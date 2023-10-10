9295b1777ffef39e7caf2421f06f8005912dfd07
tuanta <tanhtuan093@gmail.com>
2023-10-10 21:28:55 +0700

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export const TodoForm = () => {
+
+    const [value, setValue] = useState('')
+
+    return (
+        <form className='TodoForm'>
+            <input type='text' placeholder='Nhập công việc mới' className='todo-input' onChange={(e) => setValue(e.target.value)} />
+            <button type='submit' className='todo-btn'>Thêm</button>
+        </form>
+    )
+}

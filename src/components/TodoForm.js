2b1340e14720d6d6e1f4ee96c0938e3cf6b15611
tuanta <tanhtuan093@gmail.com>
2023-10-10 21:35:32 +0700

    update

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -6,7 +6,7 @@ export const TodoForm = () => {
 
     return (
         <form className='TodoForm'>
-            <input type='text' placeholder='Nhập công việc mới' className='todo-input' onChange={(e) => setValue(e.target.value)} />
+            <input type='text' placeholder='Nhập công việc mới' className='todo-input' onChange={(e) => setValue(e.target.value)} value={value}/>
             <button type='submit' className='todo-btn'>Thêm</button>
         </form>
     )
